1ab14aa9d18d6d9b0c20327459223b6f8cf0b942
Persi87 <simon_black87@hotmail.co.uk>
2021-11-29 23:25:33 +0000

    commit tree ce5a280083b4f1079a27d3ba912d68518b6cb833

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+    attr_reader :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if self.side_1 <= 0 || self.side_2 <= 0 || self.side_3 <= 0
+        raise TriangleError
+    elsif self.side_1 + self.side_2 <= self.side_3 || self.side_1 + self.side_3 <= self.side_2 || self.side_2 + self.side_3 <= self.side_1
+        raise TriangleError
+    elsif self.side_1 == self.side_2 && self.side_2 == self.side_3
+      :equilateral
+    elsif self.side_1 == self.side_2 || self.side_1 == self.side_3 || self.side_2 == self.side_3
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+    
+  class TriangleError < StandardError
+
+  end
+
 end
